8b959395157862b818426b3ea2ba3faae2a5c3ae
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-05-16 09:29:42 -0700

    Sign Up

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('venues/', views.venues_index, name='index'),
+    path('accounts/signup/', views.signup, name='signup'),
+
 ]
