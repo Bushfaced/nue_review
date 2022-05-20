89a0af2b6f052b1b1ebe1cca403e3aefdf1a0480
richardguzman <thisisrichardguzman@gmail.com>
2022-05-20 12:25:47 -0700

    last commit missed two lines needed to delete

--- main_app/views.py
+++ main_app/views.py
@@ -98,7 +98,6 @@ def add_comment(request, venue_id):
   if form.is_valid():
     new_comment = form.save(commit=False)
     new_comment.venue_id = venue_id
-    # new_comment.user_id = request.user.id
     new_comment.username = request.user.username
     new_comment.save()
   return redirect('detail', venue_id=venue_id)
